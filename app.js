5dc220e63b5bd96a11342b066942904bbd7232d1
Ivan Petrovic <55898583+petrovicivan00@users.noreply.github.com>
2022-09-13 17:33:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+/*
 // ADMIN APP
 app.use("/", routes);
 app.use(express.static(path.join(__dirname, "static")));
+*/
 
 //CLIENT APP
-/*
 const client = express.static(path.join(__dirname, 'dist'));
 app.use(history({ index: '/index.html' }));
 app.use(client);
-*/
+
 app.listen( process.env.PORT || 4000, () => {
     console.log("Backend server is running on port " + process.env.PORT);
 });
